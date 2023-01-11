e3ef70266927303b72aa03ee7c8c150f51a66a86
VitaliyTheSunflower <thelightbringer98@hotmail.com>
2023-01-11 12:42:20 +0300

    add new function

--- js/game.js
+++ js/game.js
@@ -1,8 +1,19 @@
+const choice = Math.round(Math.random() * 100 );
 
+function getComputerChoice() {
+if (choice <= 33 ) {
+    console.log("rock") 
+} else if (choice > 33 && choice <= 66){
+    console.log("paper")
+} else if (choice > 66 && choice <= 99) {
+    console.log("scissors")
+} else {
+    console.log("something is wrong :(")
+}
 
+}
 
+function playRound(playerSelection , computerSelection) {
 
-
-
-
+}
 
