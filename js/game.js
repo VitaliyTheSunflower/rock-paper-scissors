326a8e630933bea4567efca6e0188d3772725592
VitaliyTheSunflower <thelightbringer98@hotmail.com>
2023-01-12 19:56:35 +0300

    finalize js

--- js/game.js
+++ js/game.js
@@ -1,19 +1,66 @@
 const choice = Math.round(Math.random() * 100 );
+const playerSelection = prompt("Make your choice!").toLowerCase();
+const computerSelection = getComputerChoice();
+
 
 function getComputerChoice() {
 if (choice <= 33 ) {
-    console.log("rock") 
+    return "Rock" 
 } else if (choice > 33 && choice <= 66){
-    console.log("paper")
+    return "Paper"
 } else if (choice > 66 && choice <= 99) {
-    console.log("scissors")
+    return "Scissors"
 } else {
     console.log("something is wrong :(")
 }
 
 }
 
-function playRound(playerSelection , computerSelection) {
 
+
+function playRound(playerSelection , computerSelection) {
+    
+    if(playerSelection === "rock" && computerSelection === "Paper"){
+        return "You lose! Paper beats Rock."
+    
+    } else if(playerSelection === "paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock."
+    
+    } else if(playerSelection === "rock" && computerSelection === "Scissors"){
+        return " You win! Rock beats Scissors."
+    
+    } else if(playerSelection === "scissors" && computerSelection === "Rock"){
+        return "You lose! Rock beats Scissors."
+    
+    } else if(playerSelection === "rock" && computerSelection === "Rock"){
+        return "Oh! That's draw!"
+    
+    } else if(playerSelection === "paper" && computerSelection === "Paper"){
+        return "Oh! That's draw!"
+    
+    } else if(playerSelection === "scissors" && computerSelection === "Scissors"){
+        return "Oh! That's draw!"
+    
+    } else if(playerSelection === "paper" && computerSelection === "Scissors"){
+        return "You lose! Scissors beat Paper."
+    
+    } else if(playerSelection === "scissors" && computerSelection === "Paper"){
+        return "You win! Scissors beat Paper."
+    
+    } else {
+        return "Oops! Something went wrong :("
+    }
 }
+
+console.log(playRound(playerSelection,computerSelection));
+
+function game(){
+    const playerSelection = prompt("Make your choice!").toLowerCase();
+    const computerSelection = getComputerChoice();
+    for( let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection,computerSelection));
+    }
+}
+
+
 
